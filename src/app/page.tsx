b835a5b98c8e768c6f094ac7575862cc03ea15e5
Binod RD <binodatjaper@gmail.com>
2023-09-09 04:08:25 +0545

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,73 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [user, setUser] = useState({});
+    const userDetail = {
+        name: 'Binod RD',
+        email: 'binod@gmail.com',
+        phone: 9843779778,
+        contact: {
+            address: 'Hattiban',
+            city: 'Lalitpur',
+            country: 'Nepal'
+        }
+    }
+
+    const setLocalStorage = (data: any) => {
+        localStorage.setItem('user', JSON.stringify(data));
+    }
+
+    const getLocalStorage = () => {
+        let getUser: any = localStorage.getItem('user');
+        if (getUser) {
+            setUser(JSON.parse(getUser));
+        }
+    }
+
+    const clearLocalStorage = () => {
+        localStorage.clear();
+        setUser({});
+    }
+
+    return (
+        <div className="container">
+            <br />
+            <h1>User Details</h1>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>City</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phone}</td>
+                        <td>{user.contact?.address}</td>
+                        <td>{user.contact?.city}</td>
+                        <td>{user.contact?.country}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br />
+            <button onClick={() => setLocalStorage(userDetail)} className="btn btn-success">
+                Set LocalStorage
+            </button>
+            <span>&nbsp;&nbsp;</span>
+            <button onClick={getLocalStorage} className="btn btn-primary">
+                Get LocalStorage
+            </button>
+            <span>&nbsp;&nbsp;</span>
+            <button onClick={clearLocalStorage} className="btn btn-danger">
+                Clear LocalStorage
+            </button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
